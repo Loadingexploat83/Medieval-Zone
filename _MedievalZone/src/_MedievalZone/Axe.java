797b16f7c324d416477ac5d3e8baa204a972a25d
luisr <luisr@Solaris>
2025-12-13 03:11:50 +0100

    Axes v3

--- _MedievalZone/src/_MedievalZone/Axe.java
+++ _MedievalZone/src/_MedievalZone/Axe.java
@@ -12,13 +12,11 @@ public class Axe extends Weapon {
 	public Axe(int mod){
 		super(15,25);
 
-		modDmg(mod*mult);
-		modWeight(mod*mult);
+		modDmg();
 	}
-	//NIGGA
+	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return "This Weapon is an Axe With "+ getDmg() +" Attack and "+ getWeight()+ " Weight";
 	}
 
