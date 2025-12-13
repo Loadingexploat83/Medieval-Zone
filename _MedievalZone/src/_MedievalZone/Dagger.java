3d9c4d069de8c3a2a2a4b8a959a6defb3700cccf
luisr <luisr@Solaris>
2025-12-13 01:59:18 +0100

    Dagger

---
+++ _MedievalZone/src/_MedievalZone/Dagger.java
@@ -0,0 +1,22 @@
+package _MedievalZone;
+
+public class Dagger extends Weapon {
+	
+	private int mult = 2;
+	
+	public Dagger(){
+		super(5,5);
+	}
+	
+	public Dagger(int mod){
+		super(5,5);
+		modDmg(mod*mult);
+		modWeight(mod*mult);
+	}
+	
+	@Override
+	public String toString() {
+		return "Dagger";
+	}
+
+}
