1b2398905ef9068859de16e92207090eba8f2a14
luisr <luisr@Solaris>
2025-12-13 02:31:02 +0100

    Bow and Hammer
    Bow and Arrows(Arrows es un atributo de Bow dado a que seria inecesario
    el uso como consumible)
    
    Hammer es un arma normal

---
+++ _MedievalZone/src/_MedievalZone/Bow.java
@@ -0,0 +1,11 @@
+package _MedievalZone;
+
+public class Bow extends Weapon {
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ _MedievalZone/src/_MedievalZone/Hammer.java
@@ -0,0 +1,11 @@
+package _MedievalZone;
+
+public class Hammer extends Weapon {
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
