9a3b7130ee93386330cd7f9ce260ef975f6dd0b2
luisr <luisr@Solaris>
2025-12-14 00:44:15 +0100

    Sword ability
    you can now use the active ability of the sword to have chance to deal
    1000x the damge the sword but if failed will do nothing. behead()
    
    you use the getDmg() for normal attacks;

--- _MedievalZone/src/_MedievalZone/Sword.java
+++ _MedievalZone/src/_MedievalZone/Sword.java
@@ -5,6 +5,7 @@ public class Sword extends Weapon {
 	private static int mult = 5;
 	private static int baseDmg = 10;
 	private static int baseWeight = 20;
+	private int percentage = 10;
 	
 	public Sword() {
 		super(baseDmg,baseWeight);
@@ -14,7 +15,16 @@ public class Sword extends Weapon {
 		super(baseDmg,baseWeight);
 		modDmg(mod*mult);
 		modWeight(mod*mult);
-		
+	}
+	
+	public int behead() {
+		if(Math.random()*100< percentage) {
+			System.out.println("You have beheaded the Enemy");
+			return getDmg()*1000;
+		}else {
+			System.out.println("The Enemy dodge the Attack");
+			return 0;
+		}
 	}
 	
 	@Override
